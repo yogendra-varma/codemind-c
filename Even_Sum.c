c0b0a31150c2e07746275913e3cce77a2a5bb3c0
yogendra-varma <97592293+yogendra-varma@users.noreply.github.com>
2022-06-28 19:57:55 +0530

    #include<stdio.h> int main() {     int n,ar[20],sum=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(int i=0;i<n;i++)     {         if(ar[i]%2==0)         {             sum=sum+ar[i];         }     }     printf("%d",sum); }

---
+++ Even_Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,ar[20],sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]%2==0)
+        {
+            sum=sum+ar[i];
+        }
+    }
+    printf("%d",sum);
+}
