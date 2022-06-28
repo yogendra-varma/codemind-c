37d71d44b23bcc6599fc3b36e33f36d62700358e
yogendra-varma <97592293+yogendra-varma@users.noreply.github.com>
2022-06-28 08:38:50 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c;     scanf("%d",&n);     printf("%d %d ",a,b);     for(int i=3;i<=n;i++)     {         c=a+b;         printf("%d ",c);         a=b;         b=c;     } }

--- fibonacci.c
+++ fibonacci.c
@@ -1,21 +1,14 @@
-
-#include<stdio.h>    
-void printFibonacci(int n){    
-    static int n1=0,n2=1,n3;    
-    if(n>0){    
-         n3 = n1 + n2;    
-         n1 = n2;    
-         n2 = n3;    
-         printf("%d ",n3);    
-         printFibonacci(n-1);    
-    }    
-}    
-int main(){    
-    int n;    
-    scanf("%d",&n);    
-    printf("");    
-    printf("%d %d ",0,1);    
-    printFibonacci(n-2);//n-2 because 2 numbers are already printed    
-  return 0;  
- }    
-
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,c;
+    scanf("%d",&n);
+    printf("%d %d ",a,b);
+    for(int i=3;i<=n;i++)
+    {
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+}
