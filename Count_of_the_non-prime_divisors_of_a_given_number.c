a778e37082774d6c06c20efbec0b6c2d4c70981d
yogendra-varma <97592293+yogendra-varma@users.noreply.github.com>
2022-06-28 19:48:57 +0530

    #include<stdio.h> int main() {     int n,i,fact=0,j,count=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             fact=0;             for(j=1;j<=i;j++)             {                 if(i%j==0)                 {                     fact=fact+1;                 }             }             if(fact!=2)             {                 count+=1;             }         }     }     printf("%d",count);     return 0; }

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,fact=0,j,count=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            fact=0;
+            for(j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    fact=fact+1;
+                }
+            }
+            if(fact!=2)
+            {
+                count+=1;
+            }
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
