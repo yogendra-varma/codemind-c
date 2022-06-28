b3e483200cf186e160c8cd7e5844aceb184d37b9
yogendra-varma <97592293+yogendra-varma@users.noreply.github.com>
2022-06-28 19:54:53 +0530

    #include<stdio.h> int main() {     int n,ar[20],sum=0,s=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(int i=0;i<n;i++)     {         if(ar[i]%2!=0)         {             sum=sum+ar[i];         }         else         s=s+ar[i];     }     if(s>sum)     printf("%d",s-sum);     else     printf("%d",sum-s); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,ar[20],sum=0,s=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]%2!=0)
+        {
+            sum=sum+ar[i];
+        }
+        else
+        s=s+ar[i];
+    }
+    if(s>sum)
+    printf("%d",s-sum);
+    else
+    printf("%d",sum-s);
+}
