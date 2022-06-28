2697c02442034502b693db07755502d703f0886d
yogendra-varma <97592293+yogendra-varma@users.noreply.github.com>
2022-06-28 20:06:58 +0530

    #include<stdio.h> int main() {     int n,ar[100];     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(int i=n-1;i>=0;i--)     {         if(ar[i]%2!=0)         {             printf("%d",ar[i]);             break;         }     } }

---
+++ last_odd_number_in_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,ar[100];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        if(ar[i]%2!=0)
+        {
+            printf("%d",ar[i]);
+            break;
+        }
+    }
+}
