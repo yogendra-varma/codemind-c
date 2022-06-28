54b0f8b589aca803aa45d3d096265480b4739748
yogendra-varma <97592293+yogendra-varma@users.noreply.github.com>
2022-06-28 08:28:02 +0530

    #include<stdio.h> int main() {     int n,i,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         if(i*(i+1)==n)         {             flag++;         }     }     if(flag!=0)     {         printf("YES");     }else{         printf("NO");     }     return 0; }

---
+++ Pronic_Number_or_Heteromecic_Number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        if(i*(i+1)==n)
+        {
+            flag++;
+        }
+    }
+    if(flag!=0)
+    {
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+    return 0;
+}
